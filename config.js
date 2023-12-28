4fe8216c029683e2aba5ba03ce31d4437f7c7a2c
derpferpmerp <colemattfleming@gmail.com>
2023-12-27 18:48:26 -0600

    Added Search Functionality

--- config.js
+++ config.js
@@ -6,3 +6,13 @@ MathJax.Hub.Config({
     TeX: { noUndefined: { attributes: { mathcolor: "red", mathbackground: "#FFEEEE", mathsize: "90%" } } },
     messageStyle: "none"
 });
+function searchRecipes(text) {
+    const recipeDivs = document.getElementsByClassName("main-element");
+    for (const element of recipeDivs){
+        if ( !element.textContent.match(text) && text != "") {
+            element.style.display = "none";
+        } else if (text=="") {
+            element.style.display = "flex";
+        }
+    }
+}
